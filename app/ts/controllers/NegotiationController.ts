3e186cb0200e476805e9da84bdd5457b08ffc38e
paulo101977 <paulo10.1977@yahoo.com.br>
2019-03-31 21:52:12 -0300

    Initial commit

---
+++ app/ts/controllers/NegotiationController.ts
@@ -0,0 +1,25 @@
+class NegotiationController {
+
+    private _inputDate;
+    private _inputQuantity;
+    private _inputValue;
+
+    constructor() {
+
+        this._inputDate = document.querySelector('#data');
+        this._inputQuantity = document.querySelector('#quantidade');
+        this._inputValue = document.querySelector('#valor');
+    }
+
+    add(event) {
+
+        event.preventDefault();
+
+        const negotiation = new Negotiation(
+            this._inputDate.value,
+            this._inputQuantity.value,
+            this._inputValue.value);
+
+        console.log(negotiation);
+    }
+}
